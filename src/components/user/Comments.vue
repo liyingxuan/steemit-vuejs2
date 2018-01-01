eabf50b51b2eaabcd0dc88e699dd5c75b1ab2bee
liyingxuan <mvp_xuan@163.com>
2018-01-01 19:45:03 +0800

    Get my list of comments.

--- src/components/user/Comments.vue
+++ src/components/user/Comments.vue
@@ -3,8 +3,8 @@
     <profile></profile>
     <profile-top-nav></profile-top-nav>
 
-    <div class="container">
-      profile-comments
+    <div class="container comments-body">
+      <comment-my-list :commentList="myComments['data']"></comment-my-list>
     </div>
   </div>
 </template>
@@ -12,16 +12,29 @@
 <script>
   import Profile from './Profile'
   import ProfileTopNav from './ProfileTopNav'
+  import CommentMyList from './CommentMyList'
+  import {mapState} from 'vuex'
 
   export default {
 		name: "comments",
+    created() {
+      this.$store.dispatch('myCommentList')
+    },
+    computed: {
+      ...mapState({
+        myComments: state => state.Comment.myComments
+      })
+    },
     components: {
       Profile,
-      ProfileTopNav
+      ProfileTopNav,
+      CommentMyList
     }
 	}
 </script>
 
 <style scoped>
-
+  .comments-body {
+    margin-top: 30px;
+  }
 </style>
--- src/store/modules/comment.js
+++ src/store/modules/comment.js
@@ -3,12 +3,16 @@ import axios from 'axios'
 
 export default {
   state: {
+    myComments: [],
     commentList: [],
     viewComment: false
   },
   mutations: {
     [types.SET_COMMENT_LIST](state, payload) {
       state.commentList = payload.comments
+    },
+    [types.GET_MY_COMMENT](state, payload) {
+      state.myComments = payload.comments
     },
     [types.ADD_COMMENT_LIST](state, payload) {
       state.commentList.unshift(payload.comments)
@@ -25,6 +29,17 @@ export default {
           type: types.SET_COMMENT_LIST,
           comments: response.data.data.data
         })
+      })
+    },
+    myCommentList({commit}) {
+      return axios.get(process.env.API_URL + 'comment/my-comments').then(response => {
+        commit({
+          type: types.GET_MY_COMMENT,
+          comments: response.data.data
+        })
+      }).catch(error => {
+        dispatch('showNotification', {level: 'danger', msg: 'Get myCommentList error.'})
+        return false
       })
     },
     postComment({commit, dispatch}, formData) {
